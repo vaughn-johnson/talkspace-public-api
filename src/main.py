3e60fb01766bdb5413791c3219c7f1ceaf1d5c54
vaughn-johnson <vneck@uw.edu>
2020-11-13 18:44:09 -0800

    added mock / snapshot and secrets

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
-def hello_world(request):
+def get_data(request):
+    MONGO_CONNECTION_STRING = access_secret_version('talkspace-293821', 'MONGO_CONNECTION_STRING')
     """Responds to any HTTP request.
     Args:
         request (flask.Request): HTTP request object.
@@ -13,4 +14,30 @@ def hello_world(request):
     elif request_json and 'message' in request_json:
         return request_json['message']
     else:
-        return 'Hello World!'
+        return len(MONGO_CONNECTION_STRING)
+
+
+
+def access_secret_version(project_id, secret_id, version_id = 'latest'):
+    """
+    Access the payload for the given secret version if one exists. The version
+    can be a version number as a string (e.g. "5") or an alias (e.g. "latest").
+    """
+
+    # Import the Secret Manager client library.
+    from google.cloud import secretmanager
+
+    # Create the Secret Manager client.
+    client = secretmanager.SecretManagerServiceClient()
+
+    # Build the resource name of the secret version.
+    name = f"projects/{project_id}/secrets/{secret_id}/versions/{version_id}"
+
+    # Access the secret version.
+    response = client.access_secret_version(request={"name": name})
+
+    # Print the secret payload.
+    #
+    # WARNING: Do not print the secret in a production environment - this
+    # snippet is showing how to access the secret material.
+    return response.payload.data.decode("UTF-8")
