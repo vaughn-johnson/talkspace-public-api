19239f1d442aa656ac52b957d625ddcad8cb858f
Vaughn Johnson <v@ughn.io>
2020-11-14 12:25:46 -0800

    Add actual functionality! (#2)
    * Add deploy to stage
    * change response to be actual data
    * add missing dependency for pymongo
    * added test

--- src/main.py
+++ src/main.py
@@ -1,4 +1,21 @@
-def hello_world(request):
+from pymongo import MongoClient
+import pandas as pd
+import re
+import textstat
+from datetime import datetime
+from google.cloud import secretmanager
+
+PROJECT_ID = 'talkspace-293821'
+SECRET_ID = 'MONGO_CONNECTION_STRING'
+SECRETS_CLIENT = secretmanager.SecretManagerServiceClient()
+name = f"projects/{PROJECT_ID}/secrets/{SECRET_ID}/versions/latest"
+response = SECRETS_CLIENT.access_secret_version(request={"name": name})
+
+MONGO_CONNECTION_STRING = response.payload.data.decode("UTF-8")
+MONGO_CLIENT = MongoClient(MONGO_CONNECTION_STRING).talkspace.messages
+
+
+def get_data(request):
     """Responds to any HTTP request.
     Args:
         request (flask.Request): HTTP request object.
@@ -13,4 +30,83 @@ def hello_world(request):
     elif request_json and 'message' in request_json:
         return request_json['message']
     else:
-        return 'Hello World!'
+        return _get_data()
+
+
+def _get_data():
+    # Other message types include automated messages from Talkspace
+    RELEVANT_MESSAGE_TYPES = [1]
+
+    messages = pd.DataFrame([
+        *MONGO_CLIENT.find(
+            {'message_type': {'$in': RELEVANT_MESSAGE_TYPES}}
+        )
+    ])
+
+    messages.created_at = messages.created_at.apply(pd.to_datetime)
+
+    # I tend to quote my therapist and then delimit the beginning of my own
+    # words using a ">"
+    #
+    # Dallas tends to begin his messages with "Vaughn,"
+    # and ends them with "Respectfully, Dallas"
+    #
+    # Dallas: Vaughn,
+    #         lorem ipsum lakdfj
+    #         Respectfully, Dallas
+    #
+    # Vaughn: "lorem ipsum lakdfj"
+    #         > abalksjfd
+    #
+    ARROW_DELIMITER = re.compile('[^-]> ')
+    EXTRATA = re.compile('(Vaughn,\n*|Respectfully,\n\nDallas)')
+    REPEAT_NEWLINES = re.compile('\n\n+')
+
+    def extract_my_words(msg):
+        if not ARROW_DELIMITER.match(msg):
+            return msg
+
+        return ''.join(re.split(ARROW_DELIMITER, msg))[1:]
+
+    def process_message(msg):
+        msg = extract_my_words(msg)
+        msg = re.sub(EXTRATA, '', msg)
+        msg = re.sub(REPEAT_NEWLINES, '\n', msg)
+        return msg
+
+    messages.message = messages.message.apply(process_message)
+
+    # This is critical for rest of the analysis
+    messages.sort_values('created_at', axis='rows', inplace=True)
+
+    # This associates consecutive messages (in time) from the same person
+    message_block_index = messages.user_id.ne(
+        messages.user_id.shift()).cumsum()
+
+    message_blocks = messages.groupby(message_block_index).agg({
+        'message': lambda l: '\n'.join(l),
+        'created_at': min,
+        'display_name': 'first'
+    })
+
+    message_blocks['message_length'] = message_blocks.message.apply(len)
+    message_blocks['question_count'] = message_blocks.message.apply(
+        lambda x: len(re.findall(r'\?', x)))
+    message_blocks['word_count'] = message_blocks.message.apply(
+        lambda x: len(re.findall(r'\s', x)) + 1)
+    message_blocks['readability'] = message_blocks.message.apply(
+        textstat.flesch_reading_ease)
+
+    shifted_messages = message_blocks.shift().add_prefix('prev_')
+    message_blocks = pd.concat([message_blocks, shifted_messages],
+                               axis='columns')
+
+    # There are the quantities I'm interested in improving
+    message_blocks['response_time'] = (
+        message_blocks.created_at - message_blocks.prev_created_at) / pd.Timedelta(days=1)
+    message_blocks['words_per_day'] = message_blocks['word_count'] / \
+        message_blocks['response_time']
+
+    message_blocks.dropna(inplace=True)
+    formatted_response = message_blocks.drop(['message', 'prev_message'], axis=1).to_json(orient='records', date_format='iso')
+    return formatted_response
--- test/main_test.py
+++ test/main_test.py
@@ -1,2 +1,25 @@
-def test_trivial():
-    assert True
+from unittest.mock import MagicMock
+from .mock_mongo import MockPyMongo
+import json
+import sys
+import os
+
+EXPECTED_RESPONSE_FILENAME = os.path.join(os.path.dirname(__file__),
+                                          'expected_response.json')
+EXPECTED_RESPONSE = open(EXPECTED_RESPONSE_FILENAME).read()
+sys.modules['pymongo'] = MockPyMongo
+sys.modules['google.cloud'] = MagicMock()
+
+from src.main import _get_data  # noqa: E402
+
+def test_snapshot():
+    expected = _pretty_print(EXPECTED_RESPONSE)
+    observed = _pretty_print(f'{_get_data()}')
+    assert expected == observed
+
+def _pretty_print(json_string):
+    json.dumps(
+        json.loads(json_string),
+        indent=2,
+        sort_keys=True
+    )
