92a5e63e281e4e68bffa0e54426bf976a6d08981
Vaughn Johnson <v@ughn.io>
2020-11-14 19:20:38 -0800

    Add cloud storage caching (#5)
    * add cache for Google cloud storage
    * add flask as a dependency for workflow
    * changed entry point to 'refresh_data'
    * changed response content-type
    * added tests

--- src/main.py
+++ src/main.py
@@ -2,20 +2,31 @@ from pymongo import MongoClient
 import pandas as pd
 import re
 import textstat
-from datetime import datetime
+import json
+from datetime import date, datetime
 from google.cloud import secretmanager
+from google.cloud import storage
+from flask import jsonify
 
+# Get mongo connection string
 PROJECT_ID = 'talkspace-293821'
 SECRET_ID = 'MONGO_CONNECTION_STRING'
 SECRETS_CLIENT = secretmanager.SecretManagerServiceClient()
 name = f"projects/{PROJECT_ID}/secrets/{SECRET_ID}/versions/latest"
 response = SECRETS_CLIENT.access_secret_version(request={"name": name})
 
-MONGO_CONNECTION_STRING = response.payload.data.decode("UTF-8")
+# Initialize mongo client
+MONGO_CONNECTION_STRING = response.payload.data.decode('UTF-8')
 MONGO_CLIENT = MongoClient(MONGO_CONNECTION_STRING).talkspace.messages
 
+# Intialize storage client
+STORAGE_CLIENT = storage.Client()
+BUCKET = STORAGE_CLIENT.bucket('vaughn-public-talksapce-data')
 
-def get_data(request):
+def refresh_data(request):
+    return jsonify(_refresh_data())
+
+def _refresh_data():
     """Responds to any HTTP request.
     Args:
         request (flask.Request): HTTP request object.
@@ -24,13 +35,18 @@ def get_data(request):
         Response object using
         `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
     """
-    request_json = request.get_json()
-    if request.args and 'message' in request.args:
-        return request.args.get('message')
-    elif request_json and 'message' in request_json:
-        return request_json['message']
-    else:
-        return _get_data()
+    cached_filename = f'{date.today()}.json'
+
+    if BUCKET.blob(cached_filename).exists():
+        bucket_data = BUCKET.blob(cached_filename).download_as_string()
+        return json.loads(bucket_data)
+
+    data = _get_data()
+    BUCKET.blob(cached_filename).upload_from_string(
+        json.dumps(data),
+        content_type='application/json'
+    )
+    return data
 
 
 def _get_data():
@@ -81,7 +97,8 @@ def _get_data():
 
     # This associates consecutive messages (in time) from the same person
     message_block_index = messages.user_id.ne(
-        messages.user_id.shift()).cumsum()
+        messages.user_id.shift()
+    ).cumsum()
 
     message_blocks = messages.groupby(message_block_index).agg({
         'message': lambda l: '\n'.join(l),
@@ -90,16 +107,25 @@ def _get_data():
     })
 
     message_blocks['message_length'] = message_blocks.message.apply(len)
+
     message_blocks['question_count'] = message_blocks.message.apply(
-        lambda x: len(re.findall(r'\?', x)))
+        lambda x: len(re.findall(r'\?', x))
+    )
+
     message_blocks['word_count'] = message_blocks.message.apply(
-        lambda x: len(re.findall(r'\s', x)) + 1)
+        lambda x: len(re.findall(r'\s', x)) + 1
+    )
+
     message_blocks['readability'] = message_blocks.message.apply(
-        textstat.flesch_reading_ease)
+        textstat.flesch_reading_ease
+    )
 
     shifted_messages = message_blocks.shift().add_prefix('prev_')
-    message_blocks = pd.concat([message_blocks, shifted_messages],
-                               axis='columns')
+
+    message_blocks = pd.concat(
+        [message_blocks, shifted_messages],
+        axis='columns'
+    )
 
     # There are the quantities I'm interested in improving
     message_blocks['response_time'] = (
@@ -109,4 +135,4 @@ def _get_data():
 
     message_blocks.dropna(inplace=True)
     formatted_response = message_blocks.drop(['message', 'prev_message'], axis=1).to_json(orient='records', date_format='iso')
-    return formatted_response
+    return json.loads(formatted_response)
