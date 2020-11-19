d62e2b3e4d74adaad129d53d9efd31abcfcaf2e8
vaughn-johnson <vneck@uw.edu>
2020-11-18 20:24:04 -0800

    initial commit

--- src/main.py
+++ src/main.py
@@ -23,6 +23,31 @@ MONGO_CLIENT = MongoClient(MONGO_CONNECTION_STRING).talkspace.messages
 STORAGE_CLIENT = storage.Client()
 BUCKET = STORAGE_CLIENT.bucket('vaughn-public-talksapce-data')
 
+def cors_enabled_function(request):
+    # For more information about CORS and CORS preflight requests, see
+    # https://developer.mozilla.org/en-US/docs/Glossary/Preflight_request
+    # for more information.
+
+    # Set CORS headers for the preflight request
+    if request.method == 'OPTIONS':
+        # Allows GET requests from any origin with the Content-Type
+        # header and caches preflight response for an 3600s
+        headers = {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Max-Age': '3600'
+        }
+
+        return ('', 204, headers)
+
+    # Set CORS headers for the main request
+    headers = {
+        'Access-Control-Allow-Origin': '*'
+    }
+
+    return ('Hello World!', 200, headers)
+
 def refresh_data(request):
     return jsonify(_refresh_data())
 
